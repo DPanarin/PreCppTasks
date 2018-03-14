6a8bbf857239bb3aeae99dc67d19857e61b6249d
Dmitriy <panarin.de@gmail.com>
2018-03-14 14:23:47 +0200

    Add ZeroLevelProgram

---
+++ ZeroLevelProgram/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+    cout << "hello \nworld" << endl;
+    cout << 12892323+454665768 << endl;
+    return 0;
+}
