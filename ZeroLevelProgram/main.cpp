cc1482cad294cd4546d50542880f9c4f009c4751
Dmitriy <panarin.de@gmail.com>
2018-03-14 17:40:03 +0200

    Add tasks 1-7

--- ZeroLevelProgram/main.cpp
+++ ZeroLevelProgram/main.cpp
@@ -1,11 +1,56 @@
+// Hello. This is the program I wrote for the preparation for the entrance exam.
+// This program consists of separate functions for each of the 30 tasks that are described in the p2p-zero file.
+// You may call any of them by writing it number from the command line. Enjoy.
 #include <iostream>
-
 using namespace std;
 
 int main()
+//{
+//    cout << "hello world" << endl;
+//    cout << "hello \nworld" << endl;
+//    cout << 12892323+454665768 << endl;
+//    return 0;
+//}
+//{
+//    int num;
+//    cout << "Hey, enter the number: ";
+//    cin >> num;
+//    cout << "Great job! If we add one then: " << ++num;
+//    return 0;
+//}
+//{
+//    int num1, num2;
+//    cout << "Enter first number: ";
+//    cin >> num1;
+//    cout << "Enter second number: ";
+//    cin >> num2;
+//    int summ = num1 + num2;
+//    cout << "Exelent! Sum of numbers = " << summ << endl;
+//    return 0;
+//}
+//{
+//    int totalKmPeerDay;
+//    cout << "Hello. How many kilometers do you walk a day on average? ";
+//    cin >> totalKmPeerDay;
+//    if (totalKmPeerDay <= 3)
+//    {
+//        cout << "This is a very small distance" << endl;
+//    }
+//    if (totalKmPeerDay > 3 && totalKmPeerDay < 20)
+//    {
+//        cout << "Well done!" << endl;
+//    }
+////    if (totalKmPeerDay >3)
+////        if (totalKmPeerDay <20)
+////    {
+////        cout << "Well done!" << endl;
+////    }
+//    if (totalKmPeerDay >= 20)
+//    {
+//        cout << "You are marathon runner!!!" << endl;
+//    }
+//    return 0;
+//}
 {
-    cout << "hello world" << endl;
-    cout << "hello \nworld" << endl;
-    cout << 12892323+454665768 << endl;
-    return 0;
+
 }
